6282097b8461f640c95e195ba076e838932131d6
lucasmanrick <112766634+lucasmanrick@users.noreply.github.com>
2022-10-16 15:02:52 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,21 +5,22 @@ abas.forEach(aba=> {
             return
         }
 
-        const abaSelecionada = document.querySelector(".aba.selecionado");
-        abaSelecionada.classList.remove("selecionado");
-        aba.classList.add("selecionado");
+        selecaoDeAba(aba)
 
-        const informacaoSelecionada = document.querySelector (".informacao.selecionado")
-        informacaoSelecionada.classList.remove("selecionado")
-
-
-        const ElementoDeInformacaoAba = `informacao-${aba.id}`; 
-        
-        const idInformacaoASerMostrada = document.getElementById (ElementoDeInformacaoAba)
-        idInformacaoASerMostrada.classList.add ("selecionado")
-        
-        
-        
-        
-    } )
+        selecaoDeInformacao(aba)
+         } )
 });
+
+    function selecaoDeAba(aba) {const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+    aba.classList.add("selecionado");}
+
+
+    function selecaoDeInformacao(aba) {const informacaoSelecionada = document.querySelector (".informacao.selecionado")
+    informacaoSelecionada.classList.remove("selecionado")
+
+
+    const ElementoDeInformacaoAba = `informacao-${aba.id}`; 
+    
+    const idInformacaoASerMostrada = document.getElementById (ElementoDeInformacaoAba)
+    idInformacaoASerMostrada.classList.add ("selecionado")}
